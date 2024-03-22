7018172181c73bf10ae60e21a6b894c86e43afd6
Jakob <puc.jakob1@gmail.com>
2024-03-22 19:58:45 +0100

    imp

--- grafi.js
+++ grafi.js
@@ -5,10 +5,10 @@ document.addEventListener('DOMContentLoaded', function () {
     var o2 = document.getElementById('o2').getContext('2d');                           //! must be x1
     var methane = document.getElementById('methane').getContext('2d');                 //! must be x1
     var voc = document.getElementById('voc').getContext('2d');                         //! must be x1
-    //var gyro = document.getElementById('gyro').getContext('2d');                       //! must be x,y,x
-    //var acceleration = document.getElementById('acceleration').getContext('2d');       //! must be x,y,z
-    //var magnet = document.getElementById('magnet').getContext('2d');                   //!TODO must be x1 //x,y,z
-    //var height = document.getElementById('height').getContext('2d');                   //! must be x4
+    var gyro = document.getElementById('gyro').getContext('2d');                       //! must be x,y,x
+    var acceleration = document.getElementById('acceleration').getContext('2d');       //! must be x,y,z
+    var magnet = document.getElementById('magnet').getContext('2d');                   //!TODO must be x1 //x,y,z
+    var height = document.getElementById('height').getContext('2d');                   //! must be x4
     //!TODO ce bo cajt
     //var loraAcl = document.getElementById('loraAcl').getContext('2d');  
     //!sample data
@@ -18,6 +18,22 @@ document.addEventListener('DOMContentLoaded', function () {
     var pressure2 = [];
     var pressure3 = [];
     var o21 = [];
+    var voc1 = [];
+    var X = [];
+    var Y = [];
+    var Z = [];
+    var X_acel = [];
+    var Y_acel = [];
+    var Z_acel = [];
+    var X_mag = [];
+    var Y_mag = [];
+    var Z_mag = [];
+    var h1 = [];
+    var h2 = [];
+    var h3 = [];
+    var h4 = [];
+
+
 
 
     //!temparatura
@@ -157,6 +173,214 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
+    //!VOC
+    var vocChar = new Chart(voc, {
+        type: 'line',
+        data: {
+            labels: [],
+            datasets: [
+                //! spremen da bo vredu za senzor
+                {
+                    label: 'VOC',
+                    borderColor: 'rgb(0, 255, 255 )',
+                    data: voc1,
+                    fill: false
+                }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+                x: {
+                    type: 'linear',
+                    position: 'bottom'
+                },
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+
+    //!gyro
+    var gyroChart = new Chart(gyro, {
+        type: 'line',
+        data: {
+            labels: [],
+            datasets: [
+                //! spremen da bo vredu za senzor
+                {
+                    label: 'X',
+                    borderColor: 'rgb(0, 255, 255 )',
+                    data: X,
+                    fill: false
+                },
+                //! spremen da bo vredu za senzor
+                {
+                    label: 'Y',
+                    borderColor: 'rgb(255, 0, 255)',
+                    data: Y,
+                    fill: false
+                },
+                //! spremen da bo vredu za senzor
+                {
+                    label: 'Z',
+                    borderColor: 'rgb(255, 0,0)',
+                    data: Z,
+                    fill: false
+                }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+                x: {
+                    type: 'linear',
+                    position: 'bottom'
+                },
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+
+    //!acel
+    var accelerationChart = new Chart(gyro, {
+        type: 'line',
+        data: {
+            labels: [],
+            datasets: [
+                //! spremen da bo vredu za senzor
+                {
+                    label: 'X',
+                    borderColor: 'rgb(0, 255, 255 )',
+                    data: X_acel,
+                    fill: false
+                },
+                //! spremen da bo vredu za senzor
+                {
+                    label: 'Y',
+                    borderColor: 'rgb(255, 0, 255)',
+                    data: Y_acel,
+                    fill: false
+                },
+                //! spremen da bo vredu za senzor
+                {
+                    label: 'Z',
+                    borderColor: 'rgb(255, 0,0)',
+                    data: Z_acel,
+                    fill: false
+                }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+                x: {
+                    type: 'linear',
+                    position: 'bottom'
+                },
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+
+    //!Mag
+    var accelerationChart = new Chart(gyro, {
+        type: 'line',
+        data: {
+            labels: [],
+            datasets: [
+                //! spremen da bo vredu za senzor
+                {
+                    label: 'X',
+                    borderColor: 'rgb(0, 255, 255 )',
+                    data: X_mag,
+                    fill: false
+                },
+                //! spremen da bo vredu za senzor
+                {
+                    label: 'Y',
+                    borderColor: 'rgb(255, 0, 255)',
+                    data: Y_mag,
+                    fill: false
+                },
+                //! spremen da bo vredu za senzor
+                {
+                    label: 'Z',
+                    borderColor: 'rgb(255, 0,0)',
+                    data: Z_mag,
+                    fill: false
+                }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+                x: {
+                    type: 'linear',
+                    position: 'bottom'
+                },
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+
+    //!Higfht
+    var accelerationChart = new Chart(gyro, {
+        type: 'line',
+        data: {
+            labels: [],
+            datasets: [
+                //! spremen da bo vredu za senzor
+                {
+                    label: 'H1',
+                    borderColor: 'rgb(0, 255, 255 )',
+                    data: h1,
+                    fill: false
+                },
+                //! spremen da bo vredu za senzor
+                {
+                    label: 'H2',
+                    borderColor: 'rgb(255, 0, 255)',
+                    data: h2,
+                    fill: false
+                },
+                //! spremen da bo vredu za senzor
+                {
+                    label: 'H3',
+                    borderColor: 'rgb(255, 0,0)',
+                    data: h3,
+                    fill: false
+                }
+                ,
+                //! spremen da bo vredu za senzor
+                {
+                    label: 'H4',
+                    borderColor: 'rgb(255, 0,0)',
+                    data: h4,
+                    fill: false
+                }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+                x: {
+                    type: 'linear',
+                    position: 'bottom'
+                },
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
 
 
 
